a3b0975815c5dc281eaf11a31d845e704ef0dbe2
Stefan <stefan_saramet@yahoo.com>
2019-01-30 06:27:26 +0200

    Add abstract as part of the pin sent to the UI

--- ToWas.API/ToWas.API/Models/ItineraryModel.cs
+++ ToWas.API/ToWas.API/Models/ItineraryModel.cs
@@ -8,6 +8,7 @@ namespace ToWas.API.Models
         public string DescriptionUrl { get; set; }
         public string AttractionType { get; set; }
         public string AttractionTypeUrl { get; set; }
+        public string Abstract { get; set; }
         public string Country { get; set; }
         public float Latitude { get; set; }
         public float Longitude { get; set; }
@@ -17,6 +18,7 @@ namespace ToWas.API.Models
             return new ItineraryModel
             {
                 Description = pinDto.Description,
+                Abstract = pinDto.Abstract,
                 DescriptionUrl = pinDto.DescriptionUrl,
                 Country = pinDto.Country,
                 Longitude = pinDto.Longitude,
--- ToWas.API/ToWas.Rdf/Dtos/PinDto.cs
+++ ToWas.API/ToWas.Rdf/Dtos/PinDto.cs
@@ -10,22 +10,25 @@ namespace ToWas.Rdf.Dtos
         public string AttractionType { get; set; }
         public string AttractionTypeUrl { get; set; }
         public string Country { get; set; }
+        public string Abstract { get; set; }
         public float Latitude { get; set; }
         public float Longitude { get; set; }
 
         public static PinDto From(SparqlResult sparqlResult)
         {
             string graphUriAbsoluteUri = sparqlResult[0].AsValuedNode().AsString();
-            string attractionTypeUrl = sparqlResult[1].AsValuedNode().AsString();
-            string countryAbsoluteUri = sparqlResult[5].AsValuedNode().AsString();
+            string abstractDescription = sparqlResult[1].AsValuedNode().AsString();
+            string attractionTypeUrl = sparqlResult[2].AsValuedNode().AsString();
+            string countryAbsoluteUri = sparqlResult[6].AsValuedNode().AsString();
             string attractionType = sparqlResult[2].AsValuedNode().AsString();
-            float latitude = sparqlResult[4].AsValuedNode().AsFloat();
-            float longitude = sparqlResult[3].AsValuedNode().AsFloat();
+            float latitude = sparqlResult[5].AsValuedNode().AsFloat();
+            float longitude = sparqlResult[4].AsValuedNode().AsFloat();
             string description = graphUriAbsoluteUri.Remove(0,"http://dbpedia.org/resource/".Length);
 
             return new PinDto
             {
                 Description = description,
+                Abstract = abstractDescription,
                 DescriptionUrl = graphUriAbsoluteUri,
                 AttractionTypeUrl = attractionTypeUrl,
                 AttractionType = attractionType,
