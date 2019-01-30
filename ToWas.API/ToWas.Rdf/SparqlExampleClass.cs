6e46dbd0b07bf3d387a78d5ec56ed05923af904b
RazvanC88 <razvan.cazuc@yahoo.com>
2019-01-30 03:37:17 +0200

    Added query to get all cities from a country

--- ToWas.API/ToWas.Rdf/SparqlExampleClass.cs
+++ ToWas.API/ToWas.Rdf/SparqlExampleClass.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using VDS.RDF;
 using VDS.RDF.Query;
 
@@ -9,18 +10,32 @@ namespace ToWas.Rdf
     /// </summary>
     public class SparqlExampleClass
     {
-        public void DoSomething()
+        private readonly SparqlRemoteEndpoint _wikiDataEndpoint;
+        private readonly SparqlRemoteEndpoint _dbPediaEndpoint;
+
+        public SparqlExampleClass()
+        {
+            _wikiDataEndpoint = new SparqlRemoteEndpoint(new Uri("https://query.wikidata.org/sparql"), "https://query.wikidata.org");
+            _dbPediaEndpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"), "http://dbpedia.org");
+        }
+
+        public void GetAllCitiesFromCountry2()
         {
             //Define a remote endpoint
-            //Use the DBPedia SPARQL endpoint with the default Graph set to DBPedia
-            var endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"), "http://dbpedia.org");
 
             //Make a SELECT query against the Endpoint
-            var results = endpoint.QueryWithResultSet("SELECT DISTINCT ?Concept WHERE {[] a ?Concept}");
-            foreach (SparqlResult result in results)
-            {
-                Console.WriteLine(result.ToString());
-            }
+            var results = _dbPediaEndpoint.QueryWithResultSet(
+                "PREFIX dbr:    <http://dbpedia.org/resource/>\r\nPREFIX dbo:    <http://dbpedia.org/ontology/>\r\nPREFIX dct:    <http://purl.org/dc/terms/>\r\nPREFIX owl:    <http://www.w3.org/2002/07/owl#>\r\nPREFIX prov:   <http://www.w3.org/ns/prov#>\r\nPREFIX qb:     <http://purl.org/linked-data/cube#>\r\nPREFIX qudt:   <http://qudt.org/1.1/schema/qudt#>\r\nPREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\r\nPREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>\r\nPREFIX schema: <http://schema.org/>\r\nPREFIX skos:   <http://www.w3.org/2004/02/skos/core#>\r\nPREFIX unit:   <http://qudt.org/vocab/unit#>\r\nPREFIX xsd:    <http://www.w3.org/2001/XMLSchema#>\r\nPREFIX sdmx:   <http://purl.org/linked-data/sdmx#>\r\n\r\nSELECT DISTINCT ?placeName WHERE {\r\n ?placeName a yago:City108524735 ; dbo:country dbr:Croatia\r\n}");
+
+
+        }
+
+        public void GetAllCitiesFromCountry()
+        {
+            //Make a SELECT query against the Endpoint
+            var results = _wikiDataEndpoint.QueryWithResultSet("SELECT DISTINCT ?city ?cityLabel ?country ?countryLabel ?loc WHERE {\r\n?city wdt:P17 wd:Q33 .\r\n\t?city wdt:P31/wdt:P279* wd:Q515 .\r\n\t?city wdt:P17 ?country .\r\n\t?city wdt:P625 ?loc .\r\n\tSERVICE wikibase:label {\r\n\t\tbd:serviceParam wikibase:language \"en\" .\r\n\t}\r\n}");
+
+
         }
     }
 }
